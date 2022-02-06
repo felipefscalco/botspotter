4557dffe794829ffa24cdb19b3c05430e32c3398
felipefscalco <felipefscalco@gmail.com>
2022-02-05 22:29:42 -0300

    Updated OT server url

--- app/services/spotter/get_players.rb
+++ app/services/spotter/get_players.rb
@@ -2,24 +2,20 @@ module Spotter
 
   class GetPlayers
 
-    NAME = 1
-    LEVEL = 2
-    VOCATION = 3
+    NAME = 0
+    LEVEL = 1
+    VOCATION = 2
 
     def self.call
       Player.update_all online: false
 
       agent = Mechanize.new
       page = agent.get(ENV.fetch("OT_URLL"))
-      players_table = page.search('aside#right table tr')
-      players_table.shift #remove first element of array
-      players_table.shift
-      players_table.shift
-      players_table.pop
-      players_table.pop
-      players_table.pop
+      players_table = page.search('#WorldOverViewList')
+      players_info = players_table.search('tr')
+      players_info.shift
 
-      players_table.each do |player_info|
+      players_info.each do |player_info|
         info = player_info.search('td')
 
         player = {
